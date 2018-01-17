6f1856c1e0dbfc6cb49e6620dc0f97b24bb6ae9b
Rubtsov Arkady <davi9091@gmail.com>
2018-01-17 16:17:19 +0700

    added awesome button animations

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-function OpenNav() {
-  $('.sidenav').css({
-  "width": "250px"
-  });
-};
+nav = $('.sidenav');
 
-function CloseNav() {
-  $('.sidenav').css({
-    "width": "0"
-  });
+function ToggleNav() {
+  if ($('.sidenav').hasClass('closed') == true) {
+    $('.sidenav').css({
+      "width": "250px"
+    });
+
+    $('.openupper').css({
+      'top': '25px',
+      'transform': 'rotate(-45deg) translate(5px, -5px)'
+    });
+
+    $('.openlower').css({
+      'top': '45px',
+      'transform': 'rotate(45deg) translate(-18px, -19px)'
+    });
+
+    $('.openbtn').css({
+      'left': '195px'
+    });
+
+    $('.sidenav').removeClass('closed');
+  } else {
+    $('.sidenav').css({
+      "width": "0"
+    });
+
+    $('.openupper').css({
+      'top': '10px',
+      'transform': 'none'
+    });
+
+    $('.openlower').css({
+      'top': '30px',
+      'transform': 'none'
+    });
+
+    $('.openbtn').css({
+      'left': '15px'
+    });
+
+    $('.sidenav').addClass('closed');
+  }
 };
